ecae9934f0eeca43db6b6d011d30026be4127f8d
Jonathan Lawton <jonathan.lawton@chacha.com>
2014-04-25 15:32:46 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+	// ---- DEFINE CONSTANTS ----
+
+	//  ---   DIRECTORIES    ---
+	define('BASE_DIR', realpath($_SERVER["PHP_SELF"]));
+
+	//  ---       DBI        ---
+	define('DBI_SERVER','localhost');
+	define('DBI_USER','user');
+	define('DBI_PASS','password');
+	define('DBI_DB','dbname');
+	define('DBI_PREFIX','');
+?>
