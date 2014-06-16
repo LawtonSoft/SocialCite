7a4bcfa4d104b45478e9e2d1d641e6cc5de02016
Jonathan Lawton <jonathan.lawton@chacha.com>
2014-06-16 03:23:03 -0400

    Update config.php

--- config.php
+++ config.php
@@ -4,10 +4,13 @@
 	//  ---   DIRECTORIES    ---
 	define('BASE_DIR', realpath($_SERVER["PHP_SELF"]));
 
+	//  ---    FRAM3W0RK     ---
+	define('FRAM3W0RK_DIR', 'Fram3w0rk/');
+
 	//  ---       DBI        ---
-	define('DBI_SERVER','localhost');
-	define('DBI_USER','user');
-	define('DBI_PASS','password');
-	define('DBI_DB','dbname');
-	define('DBI_PREFIX','');
+	define('DBI_SERVER', 'localhost');
+	define('DBI_USER', 'user');
+	define('DBI_PASS', 'password');
+	define('DBI_DB', 'dbname');
+	define('DBI_PREFIX', '');
 ?>
