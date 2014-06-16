eec8cfc72bd26bcdf18d0756fbb1ed1908782af4
Jonathan Lawton <jonathan.lawton@chacha.com>
2014-06-16 03:24:12 -0400

    Update resource.php

--- resource.php
+++ resource.php
@@ -7,7 +7,7 @@
 	$_VARS["FILE"]['download'] = $_REQUEST["download"];
 	
 	// Pull file data
-	if(isset($_VARS["FILE"]['path']) && !empty($_VARS["FILE"]['path'])) $file = Log::$status[$DBI->execute('SELECT * FROM resource_size WHERE name = "' . $_VARS["FILE"]['path'] . '";',MYSQL_ASSOC)]['data']['result'][0];
+	if(isset($_VARS["FILE"]['path']) && !empty($_VARS["FILE"]['path'])) $file = Log::$status[$DBI->execute('SELECT * FROM resource_size WHERE url = "' . $_VARS["FILE"]['path'] . '";',MYSQL_ASSOC)]['data']['result'][0];
 	if(!isset($file) || empty($file)) {
 		header('HTTP/1.0 404 Not Found');
 		$page = Log::$status[$DBI->execute('SELECT * FROM page WHERE id = -404;',MYSQL_ASSOC)]['data']['result'][0];
@@ -20,7 +20,7 @@
 		unset($file);
 		$_VARS["FILE"]['size'] = @filesize($_VARS["FILE"]['content']);
 		
-		header("Content-type: " . $_VARS["FILE"]['type']);
+		header("Content-type: " . $_VARS["FILE"]['content_type']);
 		//header("Content-length: " . $_VARS["FILE"]['size']);
 		//header('Content-Disposition: inline; filename=' . $_VARS["FILE"]['name']);
 		if(isset($_VARS["FILE"]['download']) && (empty($_VARS["FILE"]['download']) || $_VARS["FILE"]['download'] == 'true')) header("Content-Disposition: attachment; filename=" . $_VARS["FILE"]['name']);
