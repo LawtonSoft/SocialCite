277b1b5531599360a53910fe2c93a78cb11c3b02
Jonathan Lawton <jonathan.lawton@chacha.com>
2014-06-16 03:23:55 -0400

    Update module.php

--- module.php
+++ module.php
@@ -6,6 +6,7 @@
 	if(isset($_VARS["MODULE"]['name']) && !empty($_VARS["MODULE"]['name'])) $module = Module::get($_VARS["MODULE"]["name"]);
 	if(!isset($module) || empty($module)) $page = Page::get404();
 	
+	// Check permission access
 	$_VARS["MODULE"]['permissions'] = Module::permissions($module['id']);
 	$_VARS["MODULE"]['allow'] = 1;
 	if($_VARS["MODULE"]['permissions'] != null) {
@@ -28,18 +29,17 @@
 			$_VARS["MODULE"]['allow'] = 0;
 			$page = Page::get404();
 		}
-
 	}
 	if(isset($page['id'])) {
 		$_VARS["PAGE"] = array_merge((array)$_VARS["PAGE"], (array)$page);
-		unset($page);
 		Page::template($_VARS);
 	}
 	else {
 		$_VARS["MODULE"] = array_merge((array)$_VARS["MODULE"], (array)$module);
-		unset($module);
 		header('Content-type: ' . $_VARS["MODULE"]['content_type']);
-		Module::write($_VARS["MODULE"]['name']);
+		Module::write($_VARS["MODULE"]['url']);
 	}
+	unset($page);
+	unset($module);
 	$DBI->disconnect();
 ?>
