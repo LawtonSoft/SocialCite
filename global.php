47116b6f630d8fff872190eb0055bbc2870bdb10
Jonathan Lawton <jlawton2@users.noreply.github.com>
2015-03-01 11:32:38 -0500

    Update global.php

--- global.php
+++ global.php
@@ -2,7 +2,7 @@
 	// Require Configuration File
 	require_once('config.php');
 	
-	// Require Fram3w0rk
+	// Require Fram3w0rk and necessary classes
 	require_once(FRAM3W0RK_DIR . 'index.php');
 	Instance::$import = array('class.php');
 	Instance::load();
@@ -14,16 +14,19 @@
 	$_VARS['PAGE']['request']['get'] = $_GET;
 	$_VARS['PAGE']['request']['post'] = $_POST;
 	
-	// Create default connect to DB
-	DBI::setDefaults(DBI_SERVER, DBI_USER, DBI_PASS, DBI_DB);
+	// Create defaults for DBI
+	DBI::setDefaults(array("type"=>DBI_LANG, "host"=>DBI_SERVER, "user"=>DBI_USER, "pass"=>DBI_PASS, "database"=>DBI_DB));
 	$DBI = Instance::get('DBI');
-	$id = Log::$status[$DBI->connect()];
+	Log::$status[$DBI->connect()];
+	
+	// Create defaults for FTP
+	FTP::setDefaults(FTP_SERVER, FTP_USER, FTP_PASS);
+	$FTP = Instance::get('FTP');
+	Log::$status[$FTP->connect()];
 	
 	// Pull in CMS variables
-	//$vars = Log::$status[$DBI->execute("SELECT * FROM module JOIN module_variable ON module.id = module_variable.module_id;", MYSQL_ASSOC)]['data']['result'];
 	$_VARS["SOCIALCITE"] = Module::variables('socialcite');
 	$_VARS["WEBSITE"] = Module::variables('');
-	unset($vars);
 	
 	date_default_timezone_set($_VARS["WEBSITE"]['time_zone']);
 	
