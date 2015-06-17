5d082fcabdd77c2ec5b93ba4c183bbfb532c88f6
Jonathan Lawton <jlawton2@users.noreply.github.com>
2015-06-17 00:57:28 -0400

    Update config.php
    Added error reporting.
    Defined a constant for a password hash.

--- config.php
+++ config.php
@@ -1,13 +1,20 @@
 <?php
+	// ====     DEFAULTS     ====
+	
+	// ---      PHP MODE      ---
+	error_reporting(E_STRICT);
+	
+	
 	// ==== DEFINE CONSTANTS ====
 
-	//  ---   DIRECTORIES    ---
+	//  ---    DIRECTORIES    ---
+	
+	//       BASE DIRECTORY
 	define('BASE_DIR', realpath($_SERVER["PHP_SELF"]));
-
-	//  ---    FRAM3W0RK     ---
+	//          FRAM3W0RK
 	define('FRAM3W0RK_DIR', '/var/www/Fram3w0rk/');
 
-	//  ---       DBI        ---
+	//  ---        DBI        ---
 	define('DBI_LANG', 'mysql');
 	define('DBI_SERVER', 'localhost');
 	define('DBI_USER', 'user');
@@ -19,3 +26,10 @@
 	define('FTP_SERVER', 'localhost');
 	define('FTP_USER', 'user');
 	define('FTP_PASS', 'password');
+	
+	define('CMS_MODE', 'dev');
+	
+	//  ---     SECURITY      ---
+	
+	define('PASSWORD_HASH', '');
+?>
