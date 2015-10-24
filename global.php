efc45b97ad513847ef865e8fbda47fcac25c0bfb
Jonathan Lawton <jlawton2@users.noreply.github.com>
2015-10-24 14:40:17 -0400

    Update global.php
    * Updated to reflect changes made in Fram3w0rk logging
    * Unset duplicates for $_VARS["TYPE"] and $_VARS["PATH"]
    * Made changes to the Module class

--- global.php
+++ global.php
@@ -6,6 +6,7 @@
 	require_once(FRAM3W0RK_DIR . 'index.php');
 	Instance::$import = array('class.php');
 	Instance::load();
+	Log::$blnLog = TRUE;
 	
 	// Create global array
 	$_VARS = array();
@@ -18,20 +19,39 @@
 	// Create defaults for FTP
 	FTP::setDefaults(FTP_SERVER, FTP_USER, FTP_PASS);
 	$FTP = Instance::get('FTP');
-	Log::$status[$FTP->connect()];
+	$FTP->connect();
 	
 	// Pull in CMS variables
-	$_VARS["SOCIALCITE"] = Module::variables('socialcite');
-	$_VARS["WEBSITE"] = Module::variables('');
+	$_VARS["WEBSITE"] = Module::getInfo('');
+	$_VARS["WEBSITE"] = $_VARS["WEBSITE"]["values"];
 	
 	$_VARS['PHP']['version'] = phpversion();
 	if($_VARS["WEBSITE"]['mode'] == "DEV") $_VARS['PHP']['extensions'] = get_loaded_extensions();
 	$_VARS['PAGE']['request']['get'] = $_GET;
 	$_VARS['PAGE']['request']['post'] = $_POST;
+	$_VARS['PAGE']['request']['all'] = $_REQUEST;
+	
+	// Unset rewrite variables
+	unset($_VARS['PAGE']['request']['get']["TYPE"]);
+	unset($_VARS['PAGE']['request']['post']["TYPE"]);
+	unset($_VARS['PAGE']['request']['both']["TYPE"]);
+	unset($_VARS['PAGE']['request']['get']["PATH"]);
+	unset($_VARS['PAGE']['request']['post']["PATH"]);
+	unset($_VARS['PAGE']['request']['both']["PATH"]);
+	$_VARS["TYPE"] = $_REQUEST["TYPE"];
+	$_VARS["PATH"] = $_REQUEST["PATH"];
+	
+	unset($_GET["TYPE"]);
+	unset($_POST["TYPE"]);
+	unset($_REQUEST["TYPE"]);
+	unset($_GET["PATH"]);
+	unset($_POST["PATH"]);
+	unset($_REQUEST["PATH"]);
 	
 	date_default_timezone_set($_VARS["WEBSITE"]['time_zone']);
 	
 	Instance::get('Session');
 	$_VARS['WEBSITE']['account'] = Account::verify();
-	$_VARS["WEBSITE"]['cookies'] = Log::$status[Session::getCookies()]['data'];
+	$_VARS["WEBSITE"]['cookies'] = Session::getCookies();
+	$_VARS["WEBSITE"]['cookies'] = $_VARS["WEBSITE"]['cookies']['data'];
 ?>
